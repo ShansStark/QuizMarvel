b7bb3c3ab961aa9ca02f083906f3e2ee1dbc2af2
shunmuga <shunmugalv3@gmail.com>
2023-11-15 19:20:41 +0530

    final update

--- lib/main.dart
+++ lib/main.dart
@@ -4,4 +4,4 @@ import 'package:second_application/quiz.dart';
 void main() {
   runApp(const Quiz());
 }
-//Main function : Start fof the flutter program
+//Main function : Start of the flutter program
